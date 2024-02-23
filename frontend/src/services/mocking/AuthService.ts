320208ef7218bf397968ad4579a98e92d13cc920
Valya Tureyskaya <turleo@outlook.com>
2024-02-24 00:10:42 +1000

    🚑️ Mocking types fixed

--- frontend/src/services/mocking/AuthService.ts
+++ frontend/src/services/mocking/AuthService.ts
@@ -4,5 +4,6 @@ import { type LoginSchema } from '@/types/loginSchema'
 function register(schema: RegisterSchema) {}
 function login(schema: LoginSchema) {}
 function getUser(token: string) {}
+function registerOnEvent(id: string, token: string) {}
 
 export { register, login, getUser }

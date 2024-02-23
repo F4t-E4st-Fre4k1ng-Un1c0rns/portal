8333d10959ca197550da79a058c6a34c510f2940
BeaverNotACat <beavernotacat@gmail.com>
2024-02-23 15:40:32 +1000

    Event service fix

--- event/src/api/v1/events.py
+++ event/src/api/v1/events.py
@@ -33,14 +33,14 @@ async def listevents(
     return await get_event_service().get_event_list(filter)
 
 
-@events_router.get("/{event_id}", response_model=GetEvent)
-async def get_event(event_id: UUID):
-    return await get_event_service().get_event(event_id)
-
-
 @events_router.get("/sport_types", response_model=list[SportTypeSchema])
 async def list_sport_types():
     return await get_sport_type_service().list_sport_types()
+
+
+@events_router.get("/{event_id}", response_model=GetEvent)
+async def get_event(event_id: UUID):
+    return await get_event_service().get_event(event_id)
 
 
 @events_router.post("/register", response_model=RegistrationSchema)
@@ -50,8 +50,7 @@ async def register_on_event(
     return await get_registraton_service().regiser(register.ticket_id, authorization)
 
 
-@events_router.get(
-    "/registrations/{ticket_id}", response_model=list[GetEventParticipants]
-):
+@events_router.get("/registrations/{ticket_id}",
+        response_model=list[UserProfileSchema])
 async def get_registered_esurs(ticket_id: UUID):
     return await get_registraton_service().get_registered_users(ticket_id)

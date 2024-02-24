3633a8d6d30dc776c403a4a205597696bb4ebb18
BeaverNotACat <beavernotacat@gmail.com>
2024-02-24 19:43:21 +1000

    ✨ Updated event service dump

--- event/dumps/main.sql
+++ event/dumps/main.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 15.4
--- Dumped by pg_dump version 15.4
+-- Dumped from database version 16.2 (Debian 16.2-1.pgdg120+2)
+-- Dumped by pg_dump version 16.2 (Debian 16.2-1.pgdg120+2)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -53,7 +53,8 @@ ALTER TABLE public.country OWNER TO postgres;
 CREATE TABLE public.document (
     id uuid NOT NULL,
     event_id uuid NOT NULL,
-    file character varying
+    file character varying,
+    name character varying NOT NULL
 );
 
 
@@ -182,16 +183,6 @@ ALTER TABLE public.ticket_registation OWNER TO postgres;
 --
 
 COPY public.article (id, event_id, title, text) FROM stdin;
-feec0c82-b252-45c2-b711-ed5d87417e1f	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Программа	бегат, прэс качат, протэин
-fbfa5c52-3da2-4262-9f0e-bfce856a0876	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Дистанции	большая, побольше, поменьше
-22051f2e-78bf-4c30-9499-4785790fd7c2	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Этапы	первый, второй и третий
-cc118d35-a39c-4aaa-8432-5b39b1c67559	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Награждение	медальки красивые
-f653679e-ee98-4da9-9d1b-9b30797ea632	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Схемы дистанции	схемы продуманные
-ac9f1b86-15fb-442c-9ce3-82a61389213e	bac8e7df-88f5-4da0-bf3e-f26850c132c1	Программа	педали крутим - победу забираем, потом есть
-5500632c-b80b-40e9-abbf-22711824d88e	bac8e7df-88f5-4da0-bf3e-f26850c132c1	Дистанции	марафон, 2 марафона
-1df44356-7129-46db-a6bc-b9ac5223f5c1	bac8e7df-88f5-4da0-bf3e-f26850c132c1	Этапы	первый, второй
-25db4b69-31ed-48c5-8c52-ac2276ad9f86	bac8e7df-88f5-4da0-bf3e-f26850c132c1	Награждение	велик подарим
-10b3c1ff-2d93-4480-a180-2bb3511dcbe6	bac8e7df-88f5-4da0-bf3e-f26850c132c1	Схемы дистанции	вы поймёте
 d2d53cdb-604d-4d4e-aa86-d7ec5fdfe4e2	b37d9fff-680c-4285-a2bf-740c8b8c54b4	Программа	есть
 49d80742-0a9e-4bae-98a9-68ba282cf684	b37d9fff-680c-4285-a2bf-740c8b8c54b4	Дистанции	1км, 2км, 3 км
 d9ba4da1-cba7-4732-a54f-0b1f0a0f351c	b37d9fff-680c-4285-a2bf-740c8b8c54b4	Этапы	завтрак, обед, борьба за золото
@@ -204,6 +195,14 @@ bca1ef93-ae59-4beb-968f-3c1c05c9c4cd	60f83852-df72-4110-b27e-d1d3b65dcca2	Про
 4a93d707-a7ef-457b-b8cc-a54bfa911ae2	60f83852-df72-4110-b27e-d1d3b65dcca2	Дистанции	половина Уссурийска, вокруг Уссурийска
 a5cd4645-98e6-463f-bfa8-d604e47f471a	60f83852-df72-4110-b27e-d1d3b65dcca2	Этапы	на этапы поделили
 45086fce-6d1e-4c61-b67f-8074210c5790	60f83852-df72-4110-b27e-d1d3b65dcca2	Награждение	обязательно будет
+fbfa5c52-3da2-4262-9f0e-bfce856a0876	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Дистанции	<ul>\n<li>\nЭтап кубка России по триатлону Средняя дистанция – 1,9 км плавание, 90 км велогонка, 21,1 км бег\n</li>\n<li>\nЧемпионат Любителей по триатлону Средняя дистанция – 1,9 км плавание, 90 км велогонка, 21,1 км бег\n</li>\n</ul>
+feec0c82-b252-45c2-b711-ed5d87417e1f	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Программа	<p>09:00 \tОткрытие стартово-финишного городка, начало выдачи стартовых пакетов</p>\n<p>10:00 \tТоржественное открытие "Тутаевского полумарафона "Май.Мир.Молодость."</p>\n<p>10:05 \tСтарт детского забега на 300 м</p>\n<p>10:08 \tСтарт забега безграничных возможностей на 600 м</p>\n<p>10:16 \tСтарт детского забега на 600 м</p>\n<p>10:28 \tРазминка для участников дистанции 3 км</p>\n<p>10:40 \tСтарт дистанции 3 км</p>\n<p>10:43 \tНаграждение победителей забегов на 300м, 600 м, забега безграничных возможностей</p>\n<p>11:05 \tНаграждение победителей дистанции 3 км</p>\n<p>11:17 \tРазминка для участников дистанций 10 км и 21,1 км</p>\n<p>11:30 \tСтарт дистанции 10 км</p>\n<p>11:35 \tСтарт дистанции 21,1 км</p>\n<p>12:20 \tНаграждение победителей дистанции 10 км в абсолютном зачете</p>\n<p>13:10 \tНаграждение победителей дистанции 10 км по категориям</p>\n<p>13:20 \tНаграждение победителей дистанции 21,1 км в абсолютном зачете</p>\n<p>14:15 \tНаграждение победителей дистанции 21,1 км по категориям</p>\n<p>14:45 \tОкончание бегового события</p>
+cc118d35-a39c-4aaa-8432-5b39b1c67559	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Награждение	Атлеты награждаются денежными призами, подарками организаторов и партнеров в соответствии с регламентом соревнований.
+5500632c-b80b-40e9-abbf-22711824d88e	bac8e7df-88f5-4da0-bf3e-f26850c132c1	Дистанции	<p>Определяются колличеством кругов по следующему маршруту</p>\n<img src="https://orgeo.ru/files/event/banner_top/32530_o.jpg?t=" alt="No image(" max-width="100%" loading="lazy">
+f653679e-ee98-4da9-9d1b-9b30797ea632	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Схемы дистанции	 <img src="https://rrweb.russiarunning.com/-x-/generalimages/06892590-27e6-495d-ab3a-a125261fb4ef.jpg" loading="lazy" max-width="100%" alt="No image(" >
+22051f2e-78bf-4c30-9499-4785790fd7c2	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Этапы	<h4>Плавательный этап:</h4>\n<p>\nПлавательный этап гонки будет проводиться в акватории Амурского залива. Атлетам необходимо будет преодолеть выбранную дистанцию в чистой морской воде Японского моря.\nГидрокостюмы разрешены.\n</p>\n\n<h4>Велосипедный этап:</h4>\n<p>\nВелоэтап гонки пройдёт по Федеральной трассе от полуострова Де-Фриз в сторону поселка Шмидтовка, с качественным дорожным покрытием и незначительным перепадом высот.\nРазделочные велосипеды разрешены, драфтинг - запрещен.\n</p>\n\n<h4>Беговой этап:</h4>\n<p>\nБеговой этап соревнований пройдет по уникальному низководному мосту, соединяющему полуостров Де-Фриз с районом города Владивостока – Седанка. Впервые этот мост будет открыт для проведения соревнований. Участники средней дистанции преодолеют три круга, протяжённостью 21,1 км. над морской гладью Амурского залива. Перепад высот отсутствует.\n</p>
+ac9f1b86-15fb-442c-9ce3-82a61389213e	bac8e7df-88f5-4da0-bf3e-f26850c132c1	Место проведения	<div style="position:relative;overflow:hidden;"><a href="https://yandex.ru/maps/org/russkaya_lyzhnya/44216590496/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;">Русская лыжня</a><a href="https://yandex.ru/maps/11409/primorsky-krai/category/sports_base/184107295/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:14px;">Спортивная база в Приморском крае</a><iframe src="https://yandex.ru/map-widget/v1/?ll=131.853927%2C43.058075&mode=poi&poi%5Bpoint%5D=131.854937%2C43.057646&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D44216590496&z=16.63" width="99%" height="400" loading="lazy" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe></div>
+bf68a4b2-22bc-4eab-ad06-4901911214e6	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Место проведения	<div style="position:relative;overflow:hidden;"><a href="https://yandex.ru/maps/11409/primorsky-krai/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;">Приморский край</a><a href="https://yandex.ru/maps/11409/primorsky-krai/house/posyolok_de_friz_32/ZUoHaA5oTkMGWEJuYGJzdnhhbQs=/?ll=131.991428%2C43.274450&utm_medium=mapframe&utm_source=maps&z=15.8" style="color:#eee;font-size:12px;position:absolute;top:14px;">Яндекс Карты</a><iframe src="https://yandex.ru/map-widget/v1/?ll=131.991428%2C43.274450&mode=whatshere&whatshere%5Bpoint%5D=131.987614%2C43.274293&whatshere%5Bzoom%5D=17&z=15.8" width="99%" height="400" frameborder="1" loading="lazy" allowfullscreen="true" style="position:relative;"></iframe></div>
 \.
 
 
@@ -221,7 +220,8 @@ cdac8660-86e0-49c0-9588-d7e63691d93f	Владивосток
 -- Data for Name: document; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.document (id, event_id, file) FROM stdin;
+COPY public.document (id, event_id, file, name) FROM stdin;
+ef6d0467-aa45-4662-a06c-1a40b136e1d3	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	https://russiarunning.com/file/a6f3abbb-6045-468e-a9cc-be3484c1cee2/%D0%9F%D0%BE%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%A7%D0%9F%D0%9A%20%E2%84%96%20767%20%D0%94%D0%B5-%D0%A4%D1%80%D0%B8%D0%B7%202023%20%D0%BF%D0%BE%D0%B4%D0%BF%D0%B8%D1%81%D0%B0%D0%BD.pdf/download	Положение о соревновании
 \.
 
 
@@ -230,11 +230,11 @@ COPY public.document (id, event_id, file) FROM stdin;
 --
 
 COPY public.event (id, title, about, banner, registration_start, registration_end, participation_start, participation_end, place_id, target_group_id, author_id, sport_type_id) FROM stdin;
-49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Триатлон «Де-Фриз 113». Соревнования по триатлону среди любителей и профессионалов на средней и стандартной дистанции.	Главное событие по триатлону состоится во Владивостоке, в историческом месте, на полуострове Де-Фриз.\n\nГонка «Де-Фриз 113» уже во второй раз пройдёт по живописным местам пригорода Владивостока. Беговой участок будет проходить по Низководному мосту, который соединяет полуостров Де-Фриз с городом Владивосток.\n\nСпортивное событие проходит при поддержке Министерства физической культуры и спорта, Администрации Приморского края, Федераций триатлона России и Федераций триатлона Приморского края.\n	/src/assets/images/tempory-assets/defize_banner.jpg	2023-05-31 14:00:00+00	2023-06-14 14:00:00+00	2023-06-30 14:00:00+00	2023-07-01 14:00:00+00	7915f50d-f387-4c85-bc7f-279967b93f35	39515764-58c0-401f-881d-46e976fb141d	76edff08-7def-402d-bca8-0ec70ab6d80b	2b029a3c-a3f8-4450-afbe-d05194a59ecd
-bac8e7df-88f5-4da0-bf3e-f26850c132c1	Традиционный веломарафон МТВ ХСМ "Проба сил - 2024"	Кросс-кантри веломарафон МТВ ХСМ "Проба сил -2024"	/src/assets/images/tempory-assets/velik.jpg	2023-05-31 14:00:00+00	2023-06-14 14:00:00+00	2023-06-30 14:00:00+00	2023-07-01 14:00:00+00	5f364b0f-82a4-4e5e-b6ab-dd84b496d180	f5f4d407-f0be-43ac-ac6e-fe62f0af6820	e6fc866f-c371-49d3-a11e-2dac2fa4a231	09665761-6ead-4c40-b820-27ef4d4defb3
 b37d9fff-680c-4285-a2bf-740c8b8c54b4	Зимний рогейн. Первый этап Кубка "Рогейн-ДВ"	это прекрасное мероприятие, в котором ты захочешь поучавстовать	/src/assets/images/tempory-assets/rogey_banner.jpg	2023-05-31 14:00:00+00	2023-06-14 14:00:00+00	2023-06-30 14:00:00+00	2023-07-01 14:00:00+00	dab97643-665e-476b-93be-221b829b4dc1	39515764-58c0-401f-881d-46e976fb141d	8327c2db-770d-4c0a-8cc1-5bcfe9613ac4	2b029a3c-a3f8-4450-afbe-d05194a59ecd
-d8464695-1458-48e8-900d-199c3b2ce510	Falaza Trail 2024	Однодневка или двухдневка на выбор. Двухдневка пройдет в двух зачетах: Лайт (11+11) и Про (19+18)	/src/assets/images/tempory-assets/1_banner.jpg	2023-05-31 14:00:00+00	2023-06-14 14:00:00+00	2023-06-30 14:00:00+00	2023-07-01 14:00:00+00	7dc9372c-dacc-4f21-b86c-7613e1bcea56	f5f4d407-f0be-43ac-ac6e-fe62f0af6820	8327c2db-770d-4c0a-8cc1-5bcfe9613ac4	2b029a3c-a3f8-4450-afbe-d05194a59ecd
-60f83852-df72-4110-b27e-d1d3b65dcca2	Уссури-трейл.Осень	Бег, трэйл, скайраннинг, атлетика, ходьба Межрегиональные (зональные)	/src/assets/images/tempory-assets/2_banner.jpg	2023-05-31 14:00:00+00	2023-06-14 14:00:00+00	2023-06-30 14:00:00+00	2023-07-01 14:00:00+00	698db308-6ac9-430e-8ec7-40f7747473de	f5f4d407-f0be-43ac-ac6e-fe62f0af6820	76edff08-7def-402d-bca8-0ec70ab6d80b	667dbad2-d2b6-4d52-b62c-5999837aa5e0
+49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Триатлон «Де-Фриз 113». Соревнования среди любителей и профессионалов.	Главное событие по триатлону состоится во Владивостоке, в историческом месте, на полуострове Де-Фриз.\n\nГонка «Де-Фриз 113» уже во второй раз пройдёт по живописным местам пригорода Владивостока. Беговой участок будет проходить по Низководному мосту, который соединяет полуостров Де-Фриз с городом Владивосток.\n\nСпортивное событие проходит при поддержке Министерства физической культуры и спорта, Администрации Приморского края, Федераций триатлона России и Федераций триатлона Приморского края.\n	/src/assets/images/tempory-assets/defize_banner.jpg	2023-05-31 14:00:00+00	2023-06-14 14:00:00+00	2023-06-30 14:00:00+00	2023-07-01 14:00:00+00	7915f50d-f387-4c85-bc7f-279967b93f35	39515764-58c0-401f-881d-46e976fb141d	76edff08-7def-402d-bca8-0ec70ab6d80b	2b029a3c-a3f8-4450-afbe-d05194a59ecd
+bac8e7df-88f5-4da0-bf3e-f26850c132c1	Традиционный веломарафон MTB XCM "Проба сил - 2024"	Третьи официальные соревнования по маунтинбайку на кубок Владивостока «Проба сил» стартуют в ближайшее воскресенье, 20 мая, на Русском острове. Традиционно в них примут участие велосипедисты из разных городов Приморья и Дальнего Востока – приедут спортсмены из Хабаровска, Благовещенска, Биробиджана. Всего организаторы ожидают более 100 участников	/src/assets/images/tempory-assets/velik.jpg	2023-08-09 14:00:00+00	2023-08-19 14:00:00+00	2023-09-09 14:00:00+00	2023-09-09 14:00:00+00	5f364b0f-82a4-4e5e-b6ab-dd84b496d180	39515764-58c0-401f-881d-46e976fb141d	e6fc866f-c371-49d3-a11e-2dac2fa4a231	2b029a3c-a3f8-4450-afbe-b15194a59ecd
+d8464695-1458-48e8-900d-199c3b2ce510	Falaza Trail 2024	Однодневка или двухдневка на выбор. Двухдневка пройдет в двух зачетах: Лайт (11+11) и Про (19+18)	/src/assets/images/tempory-assets/1_banner.jpg	2023-05-31 14:00:00+00	2023-06-14 14:00:00+00	2023-06-30 14:00:00+00	2023-07-01 14:00:00+00	7dc9372c-dacc-4f21-b86c-7613e1bcea56	39515764-58c0-401f-881d-46e976fb141d	8327c2db-770d-4c0a-8cc1-5bcfe9613ac4	2b029a3c-a3f8-4450-afbe-d05194a59ecd
+60f83852-df72-4110-b27e-d1d3b65dcca2	Уссури-трейл.Осень	Третья из четырех трейловых гонок серии "Уссури-трейл.Времена года-2023". Красивые виды на дистанции осенней дикой приморской тайги еще надолго останутся в вашем сердце!\nБег, трэйл, скайраннинг, атлетика, ходьба Межрегиональные (зональные)	/src/assets/images/tempory-assets/2_banner.jpg	2023-09-03 14:00:00+00	2023-10-03 14:00:00+00	2023-10-14 14:00:00+00	2023-10-16 14:00:00+00	698db308-6ac9-430e-8ec7-40f7747473de	39515764-58c0-401f-881d-46e976fb141d	76edff08-7def-402d-bca8-0ec70ab6d80b	b4917eef-8246-4feb-8eb5-b9eb818557d2
 \.
 
 
@@ -256,7 +256,8 @@ dab97643-665e-476b-93be-221b829b4dc1	913842fe-39a3-4496-be47-270236bdc30a	г. У
 --
 
 COPY public.social_link (id, event_id, link) FROM stdin;
-c9170ff6-cea3-4a29-bee2-1f1c2b6783a5	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	vk.com
+c9170ff6-cea3-4a29-bee2-1f1c2b6783a5	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	https://vk.com
+293e4d6d-3085-4cd4-9590-63f6ad5457df	bac8e7df-88f5-4da0-bf3e-f26850c132c1	https://vk.com
 \.
 
 
@@ -269,6 +270,8 @@ COPY public.sport_type (id, name, description) FROM stdin;
 2b029a3c-a3f8-4450-afbe-d05194a59ecd	Триатлон	foo
 09665761-6ead-4c40-b820-27ef4d4defb3	Лыжные гонки	foo
 667dbad2-d2b6-4d52-b62c-5999837aa5e0	Плавание	foo
+2b029a3c-a3f8-4450-afbe-b15194a59ecd	MTB марафон	foo
+b4917eef-8246-4feb-8eb5-b9eb818557d2	Трейлраннинг	foo
 \.
 
 
@@ -282,6 +285,7 @@ e353a57c-5ff9-452e-b81b-c48096182a44	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Ста
 1c13b4ff-5828-4629-ab88-e96d70e64889	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Чип для Индивидуального хронометража
 023fc81d-3c69-441b-8c1a-74b9e9f7f390	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Наклейка для гардероба
 0cf83550-a041-40af-a062-d4441e2ebeaf	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Вложения от партнеров
+690a9a75-d026-4b38-923d-ee84544a9d41	bac8e7df-88f5-4da0-bf3e-f26850c132c1	Велосипед, подходящий для езды на пересеченной местности
 \.
 
 
@@ -291,7 +295,6 @@ e353a57c-5ff9-452e-b81b-c48096182a44	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Ста
 
 COPY public.target_group (id, gender, minimal_age) FROM stdin;
 39515764-58c0-401f-881d-46e976fb141d	Любой	18
-f5f4d407-f0be-43ac-ac6e-fe62f0af6820	любой	20
 \.
 
 
@@ -300,8 +303,16 @@ f5f4d407-f0be-43ac-ac6e-fe62f0af6820	любой	20
 --
 
 COPY public.ticket (id, event_id, title, extra_title, price, max_places, sport_type_id) FROM stdin;
-84da98e9-1c91-464d-8775-4bb0f9e6dc8b	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Этап Кубка России по триатлону	113\nкм	50000	14	2b029a3c-a3f8-4450-afbe-d05194a59ecd
-15fdef20-820b-4324-81a4-61c429ff7542	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Чемпионат Приморского края, Любители	113\nкм	5000	30	2b029a3c-a3f8-4450-afbe-d05194a59ecd
+84da98e9-1c91-464d-8775-4bb0f9e6dc8b	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Этап Кубка России	113\nкм	50000	14	2b029a3c-a3f8-4450-afbe-d05194a59ecd
+15fdef20-820b-4324-81a4-61c429ff7542	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Чемпионат Любителей	113\nкм	5000	30	2b029a3c-a3f8-4450-afbe-d05194a59ecd
+2fd831e5-c79e-4e90-b75e-22aafef38588	bac8e7df-88f5-4da0-bf3e-f26850c132c1	Марафон	60\nкм	500	50	2b029a3c-a3f8-4450-afbe-b15194a59ecd
+25fdef20-820b-4324-81a4-61c429ff7542	49a5d35a-e782-4a57-897e-fd8e52bf2ea8	Зритель	Зрите\nли	500	100	09665761-6ead-4c40-b820-27ef4d4defb3
+2af6256c-01db-4567-abe9-155c8971b2ba	bac8e7df-88f5-4da0-bf3e-f26850c132c1	Зритель	Зрите\nли	0	100	2b029a3c-a3f8-4450-afbe-b15194a59ecd
+98683d61-91b5-47a5-a0e4-5502af49d9fd	bac8e7df-88f5-4da0-bf3e-f26850c132c1	Полумарафон	30\nкм	500	50	2b029a3c-a3f8-4450-afbe-b15194a59ecd
+a1f425d7-3d12-41fd-ad67-ecd9075aa4b1	60f83852-df72-4110-b27e-d1d3b65dcca2	BACKYARD20	20\nкм	0	100	b4917eef-8246-4feb-8eb5-b9eb818557d2
+72d8da7c-7780-487f-b2f4-2194603a2335	60f83852-df72-4110-b27e-d1d3b65dcca2	BACKYARD13	13\nкм	0	100	b4917eef-8246-4feb-8eb5-b9eb818557d2
+606c2ef2-d1f6-4e8b-973e-8f9789c26b2b	60f83852-df72-4110-b27e-d1d3b65dcca2	BACKYARD6	6\nкм	0	100	b4917eef-8246-4feb-8eb5-b9eb818557d2
+7efe81d7-7d2f-4d6c-a2bc-d893226f7e99	60f83852-df72-4110-b27e-d1d3b65dcca2	Зритель	Зрите\nли	0	100	b4917eef-8246-4feb-8eb5-b9eb818557d2
 \.
 
 
@@ -311,6 +322,8 @@ COPY public.ticket (id, event_id, title, extra_title, price, max_places, sport_t
 
 COPY public.ticket_registation (id, ticket_id, user_id, serial_number) FROM stdin;
 3e15345d-6c9b-493d-8d39-02232f52161b	84da98e9-1c91-464d-8775-4bb0f9e6dc8b	96e78bbd-1b7c-49d6-80a6-4b307d8b692b	23
+8af6a817-ad69-4c0f-8505-b3cd6320a1f2	84da98e9-1c91-464d-8775-4bb0f9e6dc8b	13bebca6-f67d-464c-99f2-f747e86ad8e6	2
+fc4e8048-89f9-406c-93d1-172a9e82a489	98683d61-91b5-47a5-a0e4-5502af49d9fd	13bebca6-f67d-464c-99f2-f747e86ad8e6	1
 \.
 
 
